978430ac5638a0cf5abdc301c8d8b6bc9dd95825
Testo <davidvardanyan94@gmail.com>
2021-07-24 15:09:19 +0400

    I done my second  homework

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const numberOfFilms = +prompt("How many films do you watched ?", '');
 
-// alert(`You watched ${numberOfFilms}`);
-
 const personalMovieDB = {
   count: numberOfFilms,
   movies: {},
@@ -10,14 +8,62 @@ const personalMovieDB = {
   private: false
 };
 
-const movieName = prompt("Which are the last film do you see ?", '');
-const filmEstimation = +prompt("Please estimate the film", '');
+for (let i = 0; i < 2; i++) {
+  const movieName = prompt("Which are the last film do you see ?", ''),
+    filmEstimation = +prompt("Please estimate the film", '');
 
-personalMovieDB.movies[movieName] =filmEstimation;
+  if (movieName != null && filmEstimation != null && movieName != '' && filmEstimation != '' && movieName.length < 50) {
+    personalMovieDB.movies[movieName] = filmEstimation;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
 
-const object = JSON.stringify(personalMovieDB);
+}
 
-alert(object);
+let i = 0;
+while (i < 2) {
+  const movieName = prompt("Which are the last film do you see ?", ''),
+    filmEstimation = +prompt("Please estimate the film", '');
+
+  if (movieName != null && filmEstimation != null && movieName != '' && filmEstimation != '' && movieName.length < 50) {
+    personalMovieDB.movies[movieName] = filmEstimation;
+    console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+  i++;
+}
+
+let i = 0;
+do {
+  const movieName = prompt("Which are the last film do you see ?", ''),
+  filmEstimation = +prompt("Please estimate the film", '');
+
+if (movieName != null && filmEstimation != null && movieName != '' && filmEstimation != '' && movieName.length < 50) {
+  personalMovieDB.movies[movieName] = filmEstimation;
+  console.log('done');
+} else {
+  console.log('error');
+  i--;
+}
+i++;
+
+} while (i < 2);
+
+
+if (personalMovieDB.count < 10) {
+  console.log("You watched very less movies");
+} else if (personalMovieDB.count > 9 && personalMovieDB.count < 30) {
+  console.log("You are a classic weever");
+}else if(personalMovieDB.count > 29){
+  console.log("You are a kinoman");
+}else {
+  console.log("Error");
+}
+
 console.log(personalMovieDB);
 
 
