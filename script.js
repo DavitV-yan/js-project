82f91db271f2806a1121867d21f7bbb77f05f726
Testo <davidvardanyan94@gmail.com>
2021-07-24 15:20:21 +0400

    I done my first  homework

--- script.js
+++ script.js
@@ -8,61 +8,11 @@ const personalMovieDB = {
   private: false
 };
 
-for (let i = 0; i < 2; i++) {
-  const movieName = prompt("Which are the last film do you see ?", ''),
-    filmEstimation = +prompt("Please estimate the film", '');
 
-  if (movieName != null && filmEstimation != null && movieName != '' && filmEstimation != '' && movieName.length < 50) {
-    personalMovieDB.movies[movieName] = filmEstimation;
-    console.log('done');
-  } else {
-    console.log('error');
-    i--;
-  }
+const movieName = prompt("Which are the last film do you see ?", ''),
+      filmEstimation = +prompt("Please estimate the film", '');
 
-}
-
-let i = 0;
-while (i < 2) {
-  const movieName = prompt("Which are the last film do you see ?", ''),
-    filmEstimation = +prompt("Please estimate the film", '');
-
-  if (movieName != null && filmEstimation != null && movieName != '' && filmEstimation != '' && movieName.length < 50) {
-    personalMovieDB.movies[movieName] = filmEstimation;
-    console.log('done');
-  } else {
-    console.log('error');
-    i--;
-  }
-  i++;
-}
-
-let i = 0;
-do {
-  const movieName = prompt("Which are the last film do you see ?", ''),
-  filmEstimation = +prompt("Please estimate the film", '');
-
-if (movieName != null && filmEstimation != null && movieName != '' && filmEstimation != '' && movieName.length < 50) {
-  personalMovieDB.movies[movieName] = filmEstimation;
-  console.log('done');
-} else {
-  console.log('error');
-  i--;
-}
-i++;
-
-} while (i < 2);
-
-
-if (personalMovieDB.count < 10) {
-  console.log("You watched very less movies");
-} else if (personalMovieDB.count > 9 && personalMovieDB.count < 30) {
-  console.log("You are a classic weever");
-}else if(personalMovieDB.count > 29){
-  console.log("You are a kinoman");
-}else {
-  console.log("Error");
-}
+personalMovieDB.movies[movieName] = filmEstimation;
 
 console.log(personalMovieDB);
 
